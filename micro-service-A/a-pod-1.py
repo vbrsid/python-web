4a23b71c814ed5c907c8cd802f73ce35b4265431
Vijay Bhaskar <45318961+vcloudgithub@users.noreply.github.com>
2022-03-12 21:55:55 +0530

    included dynamic content

--- micro-service-A/a-pod-1.py
+++ micro-service-A/a-pod-1.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, url_for
-import requests
+import requests, json
 
 app=Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def hello():
-	requests.get("http://a-pod-2:5002")
-	return render_template('dashboard.html')
+    resp_data=requests.get("http://a-pod-2:5002")
+    print("json:", flush=True)
+    print(resp_data.json(), flush=True)
+    data = json.loads(resp_data.content)
+    return render_template('dashboard.html', data = data)
 
 
 if __name__ == "__main__":
--- micro-service-B/a-pod-2.py
+++ micro-service-B/a-pod-2.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "this is from a-pod-2"
+    data = [
+        {"feed": "New record, over 90 views", "mins": "10", "symbol": "user"},
+        {"feed": "Database error", "mins": "15", "symbol": "bell"},
+        {"feed": "New record, over 40 views", "mins": "17", "symbol": "users"},
+        {"feed": "New comments", "mins": "25", "symbol": "comment"},
+        {"feed": "Check transactions", "mins": "28", "symbol": "bookmark"},
+        {"feed": "CPU overload", "mins": "35", "symbol": "laptop"},
+        {"feed": "New shares", "mins": "39", "symbol": "share-alt"}
+    ]
+    return json.dumps(data)
 
 
 if __name__ == "__main__":
